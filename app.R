ea8d14280730e4dc816582618a07dbbe7dbb8556
Robert Allaway <robert.j.allaway@gmail.com>
2021-02-12 21:13:04 -0800

    reorg

--- app.R
+++ app.R
@@ -234,9 +234,22 @@ server <- function(input, output, session) {
       user_teams <- syn_restGET(
         glue::glue("/user/{syn_getUserProfile()[['ownerId']]}/team?limit=10000"))$results 
       
-      ### get admin override status
-      override <- get_override_status_from_synapse_user_id(config$admin_team_table, user_teams)
-      
+      # the teams that user belongs to
+      all_teams <- purrr::map_chr(user_teams, function(x) x$id)
+        
+      # the teams with override access
+      dashboard_teams <- syn_tableQuery(glue::glue("select * from {permission_table}"))$asDataFrame()
+      allowed_teams <- sapply(dashboard_teams$TeamID, jsonlite::fromJSON)
+        
+      #final allowed agencies
+      allowed_teams <- all_teams[all_teams %in% allowed_teams]
+        
+        if(length(allowed_teams)>0){
+          override <- TRUE
+        }else{
+          override <- FALSE
+        }
+
       ### update waiter loading screen once login successful
       waiter_update(
         html = tagList(
