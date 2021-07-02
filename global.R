a4e856bf385b49120d453c878d5e846952e14bbd
rrchai <rchai@sagebase.org>
2021-07-02 17:44:16 +0000

    create a branch that uses schematic-dev

--- global.R
+++ global.R
@@ -77,7 +77,7 @@ scope <- "openid view download modify"
 
 # Activate conda env
 # Don't necessarily have to set `RETICULATE_PYTHON` env variable
-reticulate::use_condaenv("data_curator_env_oauth")
+reticulate::use_condaenv("schematic_dev")
 
 # Import functions/modules
 source_files <- list.files(c("functions", "modules"), pattern = "*\\.R$", recursive = TRUE, full.names = TRUE)
