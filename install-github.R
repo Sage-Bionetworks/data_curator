21b5a89d85d759fbb66fd35b45545b72d38c31d9
bhoff <bruce.hoff@sagebase.org>
2021-12-15 10:29:04 -0800

    add traceback to install_github

--- install-github.R
+++ install-github.R
@@ -3,4 +3,7 @@ gh <- "dreamRs/shinypop"
 
 # We will only install public repo's from Github
 Sys.unsetenv("GITHUB_PAT")
-remotes::install_github(gh, auth_token=NULL)
+tryCatch(
+	remotes::install_github(gh, auth_token=NULL),
+	error = function(e) {message(traceback())}
+)
