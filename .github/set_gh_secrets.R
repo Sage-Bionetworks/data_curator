ebc46087e3649b8b2755e6ef9c842155494f7206
rrchai <rchai@sagebase.org>
2022-01-20 20:48:16 +0000

    add validation to avoid setting secret to wrong repo

--- .github/set_gh_secrets.R
+++ .github/set_gh_secrets.R
@@ -2,13 +2,18 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
+repo <- basename(Sys.getenv("PWD"))
+
+if (repo %in% c("schematic", "data-models")) stop("You are not in the data curator folder !!!")
+
 secret_lists <- yaml::yaml.load_file(toString(args[1]))$definitions
+message("Assuming all definition files are stored in the schematic folder ...")
 
 for (f in c("synapse_config", "creds_path", "service_acct_creds")) {
   system(
     sprintf(
       "gh secret set %s < %s",
-      toupper(paste0("schematic_", f)), secret_lists[f]
+      toupper(paste0("schematic_", f)), file.path("schematic", secret_lists[f])
     )
   )
 }
@@ -19,8 +24,8 @@ for (f in c("synapse_config", "creds_path", "service_acct_creds")) {
 # 2. copy content from 'token.pickle.b64' to your github secret
 system(
   "
-  less token.pickle | base64 --wrap=0 > token.pickle.b64;
-  gh secret set SCHEMATIC_TOKEN_PICKLE < token.pickle.b64;
+  less schematic/token.pickle | base64 --wrap=0 > schematic/token.pickle.b64;
+  gh secret set SCHEMATIC_TOKEN_PICKLE < schematic/token.pickle.b64;
   "
 )
-unlink("token.pickle.b64")
+unlink("schematic/token.pickle.b64")
