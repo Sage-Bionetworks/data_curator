e6594e64b0fd01ff5b40671c3dacba05dd468052
Anthony Williams <awilliams@W137.local>
2022-02-01 14:23:10 -0800

    Change localhost references to 127.0.0.1 to match OAuth client verification documentation.

--- global.R
+++ global.R
@@ -37,7 +37,7 @@ client_secret <- toString(oauth_client$CLIENT_SECRET)
 if (interactive()) {
   # for local development
   options(shiny.port = 8100)
-  app_url <- "http://localhost:8100/"
+  app_url <- "http://127.0.0.1:8100/"
 } else {
   # deployed url
   app_url <- toString(oauth_client$APP_URL)
