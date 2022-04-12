b6ec436f1d65160506b143b55539c7ba589c210b
Rongrong Chai <rchai@sagebase.org>
2022-04-12 16:34:23 -0400

    disable great expectation

--- server.R
+++ server.R
@@ -283,7 +283,8 @@ shinyServer(function(input, output, session) {
       silent = TRUE,
       annotation_status <- metadata_model$validateModelManifest(
         inFile$raw()$datapath,
-        template_schema_name()
+        template_schema_name(),
+        restrict_rules = FALSE # whether to use great expectation
       )
     )
 
