a57c45b7ed7623dc9603e140b5c4901f7f4e406c
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2022-06-21 12:42:38 -0400

    Update errors/warning message for cross-manifest rule (#347)
    * add cross manifest validation message
    
    * modernize UI for validation results
    
    * highlight entire column if matchExactOne exists
    
    * improve UI
    
    * clean up
    
    * update return values for invalid conditions

--- server.R
+++ server.R
@@ -280,57 +280,44 @@ shinyServer(function(input, output, session) {
     # loading screen for validating metadata
     dcWaiter("show", msg = "Validating...")
 
-    try(
-      silent = TRUE,
-      annotation_status <- metadata_model$validateModelManifest(
-        inFile$raw()$datapath,
-        template_schema_name(),
-        restrict_rules = TRUE # set true to disable great expectation
+    annotation_status <-
+      tryCatch(
+        metadata_model$validateModelManifest(
+          inFile$raw()$datapath,
+          template_schema_name(),
+          restrict_rules = TRUE, # set true to disable great expectation
+          project_scope = list(project_synID)
+        ),
+        error = function(e) NULL
       )
-    )
 
     # validation messages
-    valRes <- validationResult(annotation_status, input$dropdown_template, inFile$data())
-    ValidationMsgServer("text_validate", valRes, input$dropdown_template, inFile$data())
+    validation_res <- validationResult(annotation_status, input$dropdown_template, inFile$data())
+    ValidationMsgServer("text_validate", validation_res)
 
     # if there is a file uploaded
-    if (!is.null(valRes$validationRes)) {
-
-      # output error messages as data table if it is invalid value type
-      # render empty if error is not "invaid value" type - ifelse() will not work
-      if (valRes$errorType == "Invalid Value") {
-        DTableServer("tbl_validate", valRes$errorDT,
-          rownames = FALSE, filter = "none",
-          caption = "View all the error(s) highlighted in the preview table above",
-          options = list(
-            pageLength = 50, scrollX = TRUE,
-            scrollY = min(50 * nrow(valRes$errorDT), 400), lengthChange = FALSE,
-            info = FALSE, searching = FALSE
-          )
-        )
-        show(NS("tbl_validate", "table"))
-      }
+    if (!is.null(validation_res$result)) {
 
       # highlight invalue cells in preview table
-      if (valRes$errorType == "Wrong Schema") {
+      if (validation_res$error_type == "Wrong Schema") {
         DTableServer("tbl_preview", data = inFile$data(), highlight = "full")
       } else {
         DTableServer(
           "tbl_preview",
           data = inFile$data(),
-          highlight = "partial", highlightValues = valRes$errorHighlight
+          highlight = "partial", highlightValues = validation_res$preview_highlight
         )
       }
 
-      if (valRes$validationRes == "valid") {
+      if (validation_res$result == "valid") {
         # show submit button
         output$submit <- renderUI(actionButton("btn_submit", "Submit to Synapse", class = "btn-primary-color"))
-        dcWaiter("update", msg = paste0(valRes$errorType, " Found !!! "), spin = spin_inner_circles(), sleep = 2.5)
+        dcWaiter("update", msg = paste0(validation_res$error_type, " Found !!! "), spin = spin_inner_circles(), sleep = 2.5)
       } else {
         output$val_gsheet <- renderUI(
           actionButton("btn_val_gsheet", "  Generate Google Sheet Link", icon = icon("table"), class = "btn-primary-color")
         )
-        dcWaiter("update", msg = paste0(valRes$errorType, " Found !!! "), spin = spin_pulsar(), sleep = 2.5)
+        dcWaiter("update", msg = paste0(validation_res$error_type, " Found !!! "), spin = spin_pulsar(), sleep = 2.5)
       }
     } else {
       dcWaiter("hide")
