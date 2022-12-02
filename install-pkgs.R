4570a98a30f5bffe1af2c8df41f2292276b70157
afwillia <anthony.williams@sagebase.org>
2022-12-02 09:36:50 -0800

    sync with develop branch

--- install-pkgs.R
+++ install-pkgs.R
@@ -9,7 +9,7 @@ cran <- c(
   "dplyr==1.0.7",
   "shinythemes==1.2.0",
   "shinydashboard==0.7.2",
-  "stringr==1.4.1",
+  "stringr==1.4.0",
   "DT==0.20",
   "jsonlite==1.7.3",
   "reticulate==1.23",
@@ -33,7 +33,7 @@ gh <- c(
 # The binary package distributions from R Studio dramatically speed up installation time
 # For Ubuntu 18.04 (Bionic) it's https://packagemanager.rstudio.com/all/__linux__/bionic/latest
 # For Ubuntu 20.04 (Focal)  it's https://packagemanager.rstudio.com/all/__linux__/focal/latest
-options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest", getOption("repos")))
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest", getOption("repos")))
 
 install.packages("remotes")
 invisible(
