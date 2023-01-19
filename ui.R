1728c1631fbece644c5aee5367f35d657260d90e
afwillia <anthony.williams@sagebase.org>
2023-01-19 12:25:00 -0800

    Use shinyjs to disable sidebar before user picks asset view.

--- ui.R
+++ ui.R
@@ -292,6 +292,8 @@ ui <- shinydashboardPlus::dashboardPage(
 )
 
 uiFunc <- function(req) {
+  shinyjs::useShinyjs()
+  
   if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
     authorization_url <- oauth2.0_authorize_url(api, app, scope = scope)
     return(tags$script(HTML(sprintf(
