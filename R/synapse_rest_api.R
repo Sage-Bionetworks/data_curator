32c357f87b0c36df0ab53b9c2af072586dca475c
afwillia <anthony.williams@sagebase.org>
2023-01-19 12:26:38 -0800

    Add function to get user access permissions to a synapse entity.

--- R/synapse_rest_api.R
+++ R/synapse_rest_api.R
@@ -71,3 +71,30 @@ synapse_get <- function(url = "https://repo-prod.prod.sagebase.org/repo/v1/entit
   
 }
 
+
+#' @title Check Access Permissions to a Synapse Entity
+#' @description wrapper for https://rest-docs.synapse.org/rest/GET/entity/id/access.html
+#' 
+#' @param url URL to REST API endpoint
+#' @param id Synapse ID
+#' @param access Access Type to check
+#' @param auth Synapse authentication token
+#'
+#' @export
+synapse_access <- function(url = "https://repo-prod.prod.sagebase.org/repo/v1/entity",
+                        id, access, auth) {
+  
+  if (is.null(id)) stop("id cannot be NULL")
+  req_url <- file.path(url, id, "access")
+  req <- httr::GET(req_url,
+                   httr::add_headers(Authorization=paste0("Bearer ", auth)),
+                   query = list(accessType=access))
+  
+  # Send error if unsuccessful query
+  status <- httr::http_status(req)
+  if (status$category != "Success") stop(status$message)
+  
+  cont <- httr::content(req)
+  cont$result
+  
+}
