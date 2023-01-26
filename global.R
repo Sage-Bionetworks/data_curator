6cb177f5568ff4248a279933fc29cdc10e45b648
afwillia <anthony.williams@sagebase.org>
2023-01-26 12:37:58 -0800

    Remove reticulate python from global.R

--- global.R
+++ global.R
@@ -150,7 +150,6 @@ if (dca_schematic_api == "reticulate"){
   
   # We get a '126' error (non-executable) if we don't do this:
   system("chmod -R +x .venv")
-  Sys.setenv("RETICULATE_PYTHON"=file.path(getwd(), ".venv", "bin", "python3.10"))
   #setup_synapse_driver()
   
   ## Read config.json
