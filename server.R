b1306b1040dffb3082c694c8f6ddb229fd0e4d49
afwillia <anthony.williams@sagebase.org>
2023-02-28 19:52:37 -0800

    Download and save data model before writing schematic config.

--- server.R
+++ server.R
@@ -180,6 +180,7 @@ shinyServer(function(input, output, session) {
       schematic_config$synapse$master_fileview <- selected$master_asset_view()
       schematic_config$model$input$download_url <- model_ops[names(model_ops) == selected$master_asset_view()]
       yaml::write_yaml(schematic_config, "schematic_config.yml")
+      system2("curl", schematic_config$model$input$download_url, schematic_config$model$input$location)
       setup_synapse_driver()
       syn$login(authToken = access_token, rememberMe = FALSE)
       syn_store <<- synapse_driver(access_token = access_token)
