ac9bcd66fe809f851c23548934756d4c4da1aa09
afwillia <anthony.williams@sagebase.org>
2023-03-28 13:43:01 -0700

    Switch names in asset view display the label instead of synapse id

--- ui.R
+++ ui.R
@@ -123,8 +123,8 @@ ui <- shinydashboardPlus::dashboardPage(
             selectInput(
               inputId = "dropdown_asset_view",
               label = NULL, #"Asset View:",
-              choices = setNames(dcc_config$project_name,
-                                 dcc_config$synapse_asset_view)#"Generating..."
+              choices = setNames(dcc_config$synapse_asset_view,
+                                 dcc_config$project_name)#"Generating..."
             ),
             actionButton("btn_asset_view", "Click to confirm",
                          class = "btn-primary-color"
