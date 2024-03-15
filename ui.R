0db26740fda9dfea0fdfb8fe1a3da11203c13a10
afwillia <anthony.williams@sagebase.org>
2024-03-15 09:42:28 -0700

    move empty folder warning to template selection screen

--- ui.R
+++ ui.R
@@ -203,6 +203,11 @@ ui <- shinydashboardPlus::dashboardPage(
           label = NULL,
           choices = "Generating..."
         ),
+        hidden(div(
+          id = "div_template_warn",
+          height = "100%",
+          htmlOutput("text_template_warn")
+        )),
         actionButton("btn_template_select", "Download template",
                      class = "btn-primary-color"
         ),
@@ -246,11 +251,6 @@ ui <- shinydashboardPlus::dashboardPage(
               id = "div_template_xls",
               height = "100%",
               htmlOutput("text_template_xls")
-            ),
-            div(
-              id = "div_template_warn",
-              height = "100%",
-              htmlOutput("text_template_warn")
             )
           )
         ),
