c99d53edca8f950d9f0c45c6305d28399e6170a9
afwillia <anthony.williams@sagebase.org>
2024-07-11 16:27:36 -0700

    update utils tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,6 +1,6 @@
 context("Test utils")
 
-testthat("parse_env_var handles empty string", {
+test_that("parse_env_var handles empty string", {
   expect_error(parse_env_var(""), "delimiter not in")
   expect_error(parse_env_var(Sys.getenv(".fake_env")), "delimiter not in")
 })
